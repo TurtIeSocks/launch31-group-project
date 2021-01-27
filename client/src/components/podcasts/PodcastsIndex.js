3d3d4cb49ee1848f43d0d1e532c4524432c2b120
Tom Low <thomaslsternberg@gmail.com>
2021-01-27 11:00:26 -0500

    added podcast show page feature
    Updated podcastsRouter and clientRouter, updated App.js, created PodcastShowPage component.

---
+++ client/src/components/podcasts/PodcastsIndex.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import PodcastTile from "./PodcastTile";
+
+const PodcastsIndex = (props) => {
+  const [podcasts, setPodcasts] = useState([]);
+
+  const getPodcasts = async () => {
+    try {
+      const response = await fetch("api/v1/podcasts");
+      if (!response.ok) {
+        throw new Error(`${response.status} (${response.statusText})`);
+      }
+      const body = await response.json();
+      setPodcasts(body.podcasts);
+    } catch (error) {
+      console.error(`Error in fetch: ${error.message}`);
+    }
+  };
+
+  useEffect(() => {
+    getPodcasts();
+  }, []);
+
+  const podcastTiles = podcasts.map((podcast) => {
+    return <PodcastTile key={podcast.id} podcast={podcast} />;
+  });
+
+  return <div>{podcastTiles}</div>;
+};
+
+export default PodcastsIndex;
--- server/src/routes/api/v1/podcastsRouter.js
+++ server/src/routes/api/v1/podcastsRouter.js
@@ -1,22 +1,33 @@
-import express from "express"
+import express from "express";
 
-import Podcast from "../../../models/Podcast.js"
-import PodcastSerializer from "../../../serializers/PodcastSerializer.js"
+import Podcast from "../../../models/Podcast.js";
+import PodcastSerializer from "../../../serializers/PodcastSerializer.js";
 
-const podcastsRouter = new express.Router()
+const podcastsRouter = new express.Router();
 
 podcastsRouter.get("/", async (req, res) => {
   try {
-    const podcasts = await Podcast.query()
+    const podcasts = await Podcast.query();
 
     const serializedPodcasts = podcasts.map((podcast) => {
-      return PodcastSerializer.getSummary(podcast)
-    })
+      return PodcastSerializer.getSummary(podcast);
+    });
 
-    res.status(200).json({ podcasts: serializedPodcasts })
+    res.status(200).json({ podcasts: serializedPodcasts });
   } catch (error) {
-    res.status(500).json({ error: error })
+    res.status(500).json({ error: error });
   }
-})
+});
 
-export default podcastsRouter
+podcastsRouter.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const podcast = await Podcast.query().findById(id);
+    const serializedPodcast = PodcastSerializer.getSummary(podcast);
+    res.status(200).json({ podcast: serializedPodcast });
+  } catch (err) {
+    res.status(500).json({ errors: err });
+  }
+});
+
+export default podcastsRouter;
